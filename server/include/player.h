a35f23253bcb9693dde126e86271a86c34daffb7
Capucinoxx <bendertermebc@gmail.com>
2023-05-25 11:54:17 -0400

    feat: modeling abstract player class

---
+++ server/include/player.h
@@ -0,0 +1,29 @@
+#ifndef SPACE_PLAYER_H
+#define SPACE_PLAYER_H
+
+#include <vector>
+
+template<std::size_t ROWS, std::size_t COLS>
+class player {
+public:
+  enum movement_type { UP, DOWN, LEFT, RIGHT };
+  using position_type = std::pair<int, int>;
+
+private:
+  short identifier;
+  std::string username;
+  position_type location;
+  std::vector<movement_type> trail = new std::vector<movement_type>(ROWS * COLS);
+
+public:
+  virtual movement_type update() = 0;
+  virtual ~player();
+
+  short id() const noexcept { return identifier; }
+  std::string name() const noexcept { return username; }
+
+protected:
+  virtual movement_type move() = 0;
+};
+
+#endif //SPACE_PLAYER_H
