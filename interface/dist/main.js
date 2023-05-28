3b2a624758de217fcc72174ca42ab7b4d61715aa
Capucinoxx <bendertermebc@gmail.com>
2023-05-28 18:20:02 -0400

    chore: remove dist directory

--- interface/dist/main.js
+++
@@ -1,6 +0,0 @@
-import { Player } from './core/player';
-import { HSL } from './core/color';
-const board = document.getElementById('game-canvas');
-const ctx = board.getContext('2d');
-new Player("Test", new HSL(217, 0.90, 0.61)).render(ctx);
-//# sourceMappingURL=main.js.map
