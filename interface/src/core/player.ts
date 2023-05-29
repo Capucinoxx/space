a9f412a2391355e9cc24d195ec6b9cb5c692806f
Benjamin Courchesne <42392217+Capucinoxx@users.noreply.github.com>
2023-05-28 20:06:05 -0400

    Feature - smooth player movement (#9)

--- interface/src/core/player.ts
+++ interface/src/core/player.ts
@@ -1,4 +1,5 @@
 import { HSL } from './color';
+import { Position } from './position';
 
 const SHADOW_OFFSET = 3;
 const CELL_WIDTH = 30;
@@ -11,18 +12,42 @@ class Player {
   private light_color: string;
   private dark_color: string;
 
-  private pos_x: number;
-  private pos_y: number;
+  private current_position: Position;
+  private destination_position: Position;
 
   constructor(name: string, color: HSL) {
     this.name = name
 
-    this.trace_color = color.to_rgba(0.65);
+    this.trace_color = color.adjust_luminosity(0.74).to_rgba(0.95);
     this.color = color.to_rgba(0.9);
     this.light_color = color.to_rgba(1);
     this.dark_color = color.adjust_luminosity(0.35).to_rgba(1);
 
-    this.pos_y = this.pos_x = 40;
+    this.current_position = { x: 30, y: 30 };
+    this.destination_position = this.current_position;
+  }
+
+  public move(pos: Position): void {
+    this.current_position = pos;
+  }
+
+  public get position(): Position {
+    return this.current_position;
+  }
+
+  public set destination(pos: Position) {
+    this.destination_position = pos;
+  }
+
+  public get destination(): Position {
+    return this.destination_position;
+  }
+
+  public get delta(): Position {
+    return {
+      x: this.destination_position.x - this.current_position.x,
+      y: this.destination_position.y - this.current_position.y
+    }
   }
 
   public render(ctx: CanvasRenderingContext2D): void {
@@ -31,20 +56,24 @@ class Player {
   }
 
   private render_name(ctx: CanvasRenderingContext2D): void {
+    const { x, y } = this.current_position;
+
     ctx.fillStyle = this.color;
     ctx.textAlign = 'center';
     ctx.font = '12px Arial';
 
     const y_offset = -SHADOW_OFFSET * 2;
-    ctx.fillText(this.name, this.pos_x + CELL_WIDTH / 2, this.pos_y + y_offset);
+    ctx.fillText(this.name, x + CELL_WIDTH / 2, y + y_offset);
   }
 
   private render_cube(ctx: CanvasRenderingContext2D): void {
+    const { x, y } = this.current_position;
+
     ctx.fillStyle = this.dark_color;
-    ctx.fillRect(this.pos_x, this.pos_y, CELL_WIDTH, CELL_WIDTH);
+    ctx.fillRect(x, y, CELL_WIDTH, CELL_WIDTH);
 
     ctx.fillStyle = this.light_color;
-    ctx.fillRect(this.pos_x - 1, this.pos_y - SHADOW_OFFSET, CELL_WIDTH + 2, CELL_WIDTH);
+    ctx.fillRect(x - 1, y - SHADOW_OFFSET, CELL_WIDTH + 2, CELL_WIDTH);
   }
 }
 
--- interface/src/main.ts
+++ interface/src/main.ts
@@ -1,7 +1,39 @@
 import { Player } from './core/player'
 import { HSL } from './core/color'
+import { Position } from './core/position'
 
 const board = document.getElementById('game-canvas') as HTMLCanvasElement;
 const ctx = board.getContext('2d') as CanvasRenderingContext2D;
 
-new Player("Test", new HSL(217, 0.90, 0.61)).render(ctx);
+
+const player = new Player("Test", new HSL(217, 0.90, 0.61));
+player.render(ctx);
+
+function animate() {
+  ctx.clearRect(0, 0, board.width, board.height);
+  player.render(ctx);
+
+  requestAnimationFrame(animate);
+}
+
+function moveRandomly(position: Position): Position {
+  const directions = ["x+", "x-", "y+", "y-"];
+  const randomDirection = directions[Math.floor(Math.random() * directions.length)];
+
+  switch (randomDirection) {
+    case "x+": position.x += 30; break;
+    case "x-": position.x -= 30; break;
+    case "y+": position.y += 30; break;
+    case "y-": position.y -= 30; break;
+    default: break;
+  }
+
+  return position;
+}
+
+setInterval(() => {
+  player.destination = moveRandomly(player.destination)
+  player.move(player.destination)
+}, 300);
+
+animate();
