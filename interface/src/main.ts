6a2bd9889872fc0ee71d43b2a691a24911b533ab
Capucinoxx <coub2802@usherbrooke.ca>
2023-06-04 10:45:09 -0400

    feat: reduce memory usage

--- interface/src/main.ts
+++ interface/src/main.ts
@@ -1,12 +1,5 @@
-import { Player } from './core/player'
-import { HSL } from './core/color'
-import { Position } from './core/position'
+import { render_with_data } from './core/game'
 import { WebsocketService } from './service/websocket';
-
-const ws = new WebsocketService('ws://localhost:8030/spectate');
-ws.subscribe((message: string) => {
-  console.log(message);
-});
 
 const board = document.getElementById('game-canvas') as HTMLCanvasElement;
 const ctx = board.getContext('2d') as CanvasRenderingContext2D;
@@ -15,34 +8,5 @@ board.width = board.offsetWidth;
 board.height = board.offsetHeight;
 
 
-const player = new Player("Test", new HSL(217, 0.90, 0.61));
-player.render(ctx);
-
-function animate() {
-  ctx.clearRect(0, 0, board.width, board.height);
-  player.render(ctx);
-
-  requestAnimationFrame(animate);
-}
-
-function moveRandomly(position: Position): Position {
-  const directions = ["x+", "x-", "y+", "y-"];
-  const randomDirection = directions[Math.floor(Math.random() * directions.length)];
-
-  switch (randomDirection) {
-    case "x+": position.x += 30; break;
-    case "x-": position.x -= 30; break;
-    case "y+": position.y += 30; break;
-    case "y-": position.y -= 30; break;
-    default: break;
-  }
-
-  return position;
-}
-
-setInterval(() => {
-  player.destination = moveRandomly(player.destination)
-  player.move(player.destination)
-}, 300);
-
-animate();
+const ws = new WebsocketService('ws://localhost:8080/spectate');
+ws.subscribe((data: ArrayBuffer) => { render_with_data(ctx, data); });
