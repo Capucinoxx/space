9892096aef80be6944b9ab525beb2c5f62b394c6
Capucinoxx <coub2802@usherbrooke.ca>
2023-06-04 12:29:37 -0400

    chore: handle serialized frame informations

--- bot.py
+++ bot.py
@@ -9,13 +9,15 @@ class Player:
     def __init__(self):
         self.id: str | None = None
         self.pos: Tuple[int, int] | None = None
+        self.alive: int | None = None
         self.trail: List[tuple[int, int]] = []
 
     
     def deserialize(self, data: bytes) -> None:
         self.id = data[:self.UUID_SIZE]
         self.pos = struct.unpack('<II', data[self.UUID_SIZE:self.UUID_SIZE + 8])
-        data = data[self.UUID_SIZE + 8:]
+        self.alive = struct.unpack('<I', data[self.UUID_SIZE + 8:self.UUID_SIZE + 12])[0]
+        data = data[self.UUID_SIZE + 12:]
 
         count = struct.unpack("<B", data[:1])[0]
         data = data[1:]
@@ -25,7 +27,7 @@ class Player:
             data = data[8:]
 
     def __str__(self) -> str:
-        return f"Player(id={self.id}, pos={self.pos}, trail={self.trail})"
+        return f"Player(id={self.id}, pos={self.pos}, alive={self.alive}, trail={self.trail})"
 
 
 class Deserializer:
@@ -35,23 +37,23 @@ class Deserializer:
 
     def deserialize(self) -> Tuple[int, int, List[Player]]:
         print(f"Deserializing {len(self.__data)}")
-        rows, cols = struct.unpack('<II', self.__data[self.__offset:self.__offset + 8])
-        self.__offset += 8
+        rows, cols, frame = struct.unpack('<III', self.__data[self.__offset:self.__offset + 12])
+        self.__offset += 12
 
         players: List[Player] = []
         while self.__offset < len(self.__data):
             player = Player()
             player.deserialize(self.__data[self.__offset:])
             players.append(player)
-            self.__offset += Player.UUID_SIZE + 8 + 1 + len(player.trail) * 8
+            self.__offset += Player.UUID_SIZE + 12 + 1 + len(player.trail) * 8
 
         return rows, cols, players
 
 def receive_response(ws):
     data = ws.recv_frame().data
     print(f"Received {len(data)}")
-    # rows, cols, players = Deserializer(data).deserialize()
-    # print(f"Received {rows} {cols}", ", ".join([f"{player}" for player in players]))
+    rows, cols, players = Deserializer(data).deserialize()
+    print(f"Received {rows} {cols}", ", ".join([f"{player}" for player in players]))
 
 def send_request(ws):
     request = "\x03"
