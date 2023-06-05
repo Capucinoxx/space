c7368e968638f414b004a33274022b6fa35ecdf0
Capucinoxx <coub2802@usherbrooke.ca>
2023-06-05 17:10:43 -0400

    feat: refactor player movement

--- server/include/player.h
+++ server/include/player.h
@@ -2,7 +2,7 @@
 #define SPACE_PLAYER_H
 
 #include "tile_map.h"
-
+#include "grid.h"
 #include "utils.h"
 
 #include <vector>
@@ -24,164 +24,134 @@ public:
   enum direction { UP, DOWN, LEFT, RIGHT };
   using movement_type = TileMap::stmt;
 
-  using position = std::pair<uint32_t, uint32_t>;
-  using trail_iterator = typename std::vector<position>::iterator;
-  using const_trail_iterator = typename std::vector<position>::const_iterator;
+  using position = Grid<ROWS, COLS>::position;
 
-  static const int UUID_SIZE = 15;
+  static constexpr std::size_t NAME_SIZE = 15;
 
 private:
   static constexpr uint32_t MAX_SIZE = (ROWS > COLS) ? ROWS : COLS;
 
-  std::string uuid;
-  uint8_t game_manager_idx = 1; // todo use game_manager idx
-  direction next_direction;
-  position current_pos;
-  position next_position;
-  std::vector<position> trail;
-
-  std::unordered_set<std::pair<uint32_t, uint32_t>, PairHash> region;
-  std::mutex region_mutex;
-
+  std::string name;
+  uint8_t game_idx = 1;
+  uint32_t last_frame_played;
   uint32_t frame_alive = 0;
+  position current_pos;
+  direction last_direction;;
+  std::shared_ptr<Grid<ROWS, COLS>> grid;
 
+  std::unordered_set<position, PairHash> trail;
+  std::unordered_set<position, PairHash> region;
+  std::mutex region_mu;
 
 public:
-  Player(const std::string& uuid) : uuid{ uuid } {
-    // temporaire
-    next_direction = DOWN;
-    trail.reserve(MAX_SIZE);
+  Player(const std::string& name, uint32_t frame, std::shared_ptr<Grid<ROWS, COLS>> grid) 
+    : name{ name }, last_frame_played{ frame }, grid(std::move(grid)) {
+    trail.reserve(ROWS * COLS / 2);
     region.reserve(MAX_SIZE * 2);
+    last_direction = direction::DOWN;
   }
 
-  static direction parse_action(const std::string& data) {
+  std::string player_name() const noexcept { return name; }
+  uint8_t game_id() const noexcept         { return game_idx; }
+  position pos() const noexcept            { return current_pos; }
+
+  static direction const parse_action(const std::string& data) {
     switch (static_cast<uint8_t>(data[0])) {
-      case 0: return UP;
-      case 1: return DOWN;
-      case 2: return LEFT;
-      case 3: return RIGHT;
-      default: return LEFT;
+      case 0: return direction::UP;
+      case 1: return direction::DOWN;
+      case 2: return direction::LEFT;
+      case 3: return direction::RIGHT;
+      default: return direction::DOWN;
     }
   }
 
-  void set_direction(direction d) noexcept {
-    next_direction = d;
-  }
-
-  void append_region(position p) noexcept {
-    std::lock_guard<std::mutex> lock(region_mutex);
-    region.insert(p);
-  }
-
-  void set_position(position p) noexcept {
-    current_pos = p;
-  }
-
-  double frame_score() noexcept {
-    if (frame_alive == 0)
-      return 0.0;
+  double frame_score() const noexcept {
+    if (frame_alive == 0) return 0.0;
 
     double percentage = static_cast<double>(region.size()) / static_cast<double>(ROWS * COLS) * 100.00;
     return percentage * percentage / static_cast<double>(frame_alive);
   }
 
-  uint8_t idx() const noexcept { return game_manager_idx; }
-
-  std::string id() const noexcept { return uuid; }
-  position pos() const noexcept   { return current_pos; }
-
-  trail_iterator begin() noexcept { return trail.begin(); }
-  trail_iterator end() noexcept   { return trail.end(); }
-
-  const_trail_iterator begin() const noexcept { return trail.begin(); }
-  const_trail_iterator end() const noexcept   { return trail.end(); }
-
-  movement_type update() {
-    std::cout << "Player [" << uuid << "] score tick(" << frame_alive << ") = " << frame_score() << std::endl;
-    ++frame_alive;
-
-    auto res = move(next_direction);
-
-    if (region.find(current_pos) != region.end())
+  movement_type perform(uint32_t frame, direction d) {
+    if (frame <= last_frame_played)
       return movement_type::IDLE;
 
+    last_frame_played = frame;
+
+    ++frame_alive;
+
+    auto res = move(d);
 
     if (res == movement_type::IDLE)
       return res;
 
-    trail.push_back(current_pos);
+    if (trail.find(current_pos) != trail.end())
+      return movement_type::DEATH;
+
+    trail.insert(current_pos);
+
+    if (region.find(current_pos) != region.end())
+      return movement_type::COMPLETE;
 
     return res;
   }
 
+  void spawn(position p) {
+    for (int i = -1; i != 2; ++i) {
+      for (int j = -1; j != 2; ++j) {
+        auto pos = std::make_pair(p.first + i, p.second + j);
+        if (is_out_of_bound(pos))
+          continue;
+
+        grid->at(pos).step(game_id());
+        region.insert(pos);
+      }
+    }
+
+    current_pos = p;
+  }
+
   void serialize(std::vector<uint8_t>& data) {
-    serialize_data<std::string>(data, id(), UUID_SIZE);
+    serialize_data<std::string>(data, player_name(), NAME_SIZE);
     serialize_value<uint32_t>(data, pos().first);
     serialize_value<uint32_t>(data, pos().second);
     serialize_value<uint32_t>(data, frame_alive);
 
-    // serialize trail
     serialize_value<uint8_t>(data, static_cast<uint8_t>(trail.size()));
-    for (auto it = begin(); it != end(); ++it) {
+    for (auto it = trail.begin(); it != trail.end(); ++it) {
       serialize_value<uint32_t>(data, it->first);
       serialize_value<uint32_t>(data, it->second);
     }
 
-    // serialize region
-    std::lock_guard<std::mutex> lock(region_mutex);
-    {
-      serialize_value<uint32_t>(data, static_cast<uint8_t>(region.size()));
-      for (auto it = region.begin(); it != region.end(); ++it) {
-        serialize_value<uint32_t>(data, it->first);
-        serialize_value<uint32_t>(data, it->second);
-      }
+    serialize_value<uint32_t>(data, static_cast<uint8_t>(region.size()));
+    for (auto it = region.begin(); it != region.end(); ++it) {
+      serialize_value<uint32_t>(data, it->first);
+      serialize_value<uint32_t>(data, it->second);
     }
   }
 
 private:
   movement_type move(direction d) noexcept {
-    // todo: if out of bound, return IDLE
+    auto new_pos = current_pos;
+
     switch (d) {
-      case UP:    
-        --current_pos.second; 
-        if (is_out_of_bound()) {
-          ++current_pos.second;
-          return movement_type::IDLE;
-        }
-        break;
-
-      case DOWN:  
-        ++current_pos.second; 
-        if (is_out_of_bound()) {
-          --current_pos.second;
-          return movement_type::IDLE;
-        }
-        break;
-
-      case LEFT:  
-        --current_pos.first;
-        if (is_out_of_bound()) {
-          ++current_pos.first;
-          return movement_type::IDLE;
-        }
-        break;
-
-      case RIGHT:
-        ++current_pos.first;
-        if (is_out_of_bound()) {
-          --current_pos.first;
-          return movement_type::IDLE;
-        }
-        break;
+      case UP:    --new_pos.second; break;
+      case DOWN:  ++new_pos.second; break;
+      case LEFT:  --new_pos.first; break;
+      case RIGHT: ++new_pos.first; break;
     }
 
-    // todo: on aurait besoin de la map pour savoir le type de mouvement fait
-    return movement_type::STEP;
+    if (is_out_of_bound(new_pos))
+      return TileMap::stmt::IDLE;
+
+    current_pos = new_pos;
+    return TileMap::stmt::STEP;
   }
 
-  bool is_out_of_bound() const noexcept {
-    return current_pos.first >= ROWS || current_pos.second >= COLS;
+  bool is_out_of_bound(const position& pos) const noexcept {
+    return pos.first >= ROWS || pos.second >= COLS;
   }
 };
+
 
 #endif //SPACE_PLAYER_H
