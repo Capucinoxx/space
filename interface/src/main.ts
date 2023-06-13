fb8991b206a5a9e48347583ddfdec0645a858370
Capucinoxx <coub2802@usherbrooke.ca>
2023-06-13 15:34:09 -0400

    feat: link scoreboard generation

--- interface/src/main.ts
+++ interface/src/main.ts
@@ -2,6 +2,7 @@ import { render_with_data } from './core/game'
 import { WebsocketService } from './service/websocket';
 
 const board = document.getElementById('game-canvas') as HTMLCanvasElement;
+const scoreboard = document.getElementById('scoreboard') as HTMLElement;
 const ctx = board.getContext('2d') as CanvasRenderingContext2D;
 
 board.width = board.offsetWidth;
@@ -9,4 +10,4 @@ board.height = board.offsetHeight;
 
 
 const ws = new WebsocketService('ws://localhost:8080/spectate');
-ws.subscribe((data: ArrayBuffer) => { render_with_data(ctx, data); });
+ws.subscribe((data: ArrayBuffer) => { render_with_data(ctx, scoreboard, data); });
