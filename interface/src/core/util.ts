c4af76ed9f499aa3a2ccf385c3b791f0935c87fc
Capucinoxx <coub2802@usherbrooke.ca>
2023-06-13 15:30:54 -0400

    chore: method get sorted id

---
+++ interface/src/core/util.ts
@@ -0,0 +1,7 @@
+
+const get_index_array_sorted_by_values = (vals: number[]): number[] => {
+    const idx_val = vals.map((v: number, i: number) => ({ v, i }));
+    idx_val.sort((a, b) => a.v - b.v);
+
+    return idx_val.map((obj) => obj.i);
+};
