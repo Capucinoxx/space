4e28a76b56cf183f66a79e88507fb58d62449bea
Capucinoxx <coub2802@usherbrooke.ca>
2023-06-14 15:40:49 -0400

    chore: styling for score

--- interface/src/core/util.ts
+++ interface/src/core/util.ts
@@ -1,7 +1,7 @@
 
 const get_index_array_sorted_by_values = (vals: number[]): number[] => {
     const idx_val = vals.map((v: number, i: number) => ({ v, i }));
-    idx_val.sort((a, b) => a.v - b.v);
+    idx_val.sort((a, b) => b.v - a.v);
 
     return idx_val.map((obj) => obj.i);
 };
