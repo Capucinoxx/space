6dcee801e97df1eadcb8a29eb1da40d58eca1909
Capucinoxx <coub2802@usherbrooke.ca>
2023-06-15 00:31:20 -0400

    feature: connect psql instance

--- server/include/postgres_connector.h
+++ server/include/postgres_connector.h
@@ -1,21 +1,42 @@
 #ifndef SPACE_POSTGRES_H
 #define SPACE_POSTGRES_H
 
+#include "configuration.h"
 #include <pqxx/pqxx>
+#include <sstream>
 
 class PostgresConnector {
 public:
   PostgresConnector(const PostgresConnector&) = delete;
   PostgresConnector& operator=(const PostgresConnector&) = delete;
 
+  static PostgresConnector& get_instance(const Config& cfg) {
+    static PostgresConnector singleton(cfg);
+    return singleton;
+  }
+
 private:
   static PostgresConnector singleton;
   std::unique_ptr<pqxx::connection> connection;
 
-  PostgresConnector(const std::string& conn_str) {
-    connection = std::make_unique<pqxx::connection>(conn_str);
+  PostgresConnector(const Config& cfg) {
+    std::string host = cfg.at("HOST");
+    std::string port = cfg.at("PORT");
+    std::string user = cfg.at("USER");
+    std::string password = cfg.at("PASSWORD");
+    std::string dbname = cfg.at("DATABASE");
+
+    std::stringstream ss;
+    ss << "postgresql://" << user 
+       << ":" << password 
+       << "@" << host 
+       << ":" << port 
+       << "/" << dbname;
+
+    connection = std::make_unique<pqxx::connection>(ss.str());
   }
 
+public:
   pqxx::result execute_query(const std::string& query) {
     pqxx::work w(*connection);
 
@@ -25,6 +46,10 @@ private:
     w.commit();
 
     return result;
+  }
+ 
+  bool connected() const noexcept {
+    return connection->is_open();
   }
 };
 
