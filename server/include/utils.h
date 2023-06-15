9806270b8f85de4d2795b940aaab55a27fc632fb
Benjamin Courchesne <42392217+Capucinoxx@users.noreply.github.com>
2023-06-14 23:44:55 -0400

    Feature - generate UUID (#30)

--- server/include/utils.h
+++ server/include/utils.h
@@ -1,6 +1,11 @@
 #ifndef SPACE_UTILS_H
 #define SPACE_UTILS_H
 
+#include <iostream>
+#include <sstream>
+#include <random>
+#include <iomanip>
+#include <ctime>
 #include <vector>
 
 template<typename T>
@@ -11,8 +16,30 @@ void serialize_value(std::vector<uint8_t>& data, const T& value) {
 
 template<typename T>
 static void serialize_data(std::vector<uint8_t>& data, const T& value, size_t length) {
-  const uint8_t* pValue = reinterpret_cast<const uint8_t*>(value.data());
-  data.insert(data.end(), pValue, pValue + length);
+  const uint8_t* p_value = reinterpret_cast<const uint8_t*>(value.data());
+  data.insert(data.end(), p_value, p_value + length);
 }
+
+template<std::size_t LENGTH>
+class UniqueIDGenerator {
+private:
+  std::size_t count = 0;
+  std::mt19937 gen;
+
+  UniqueIDGenerator() : gen(std::random_device()()) {}
+
+public:
+  std::string operator()() {
+    std::uniform_int_distribution<> dis('A', 'Z');
+
+    std::stringstream ss;
+    ss << sts::setw(3) << std::setfill('0') << ++count;
+
+    while (ss.str().size() < LENGTH)
+      ss << static_cast<char>(dis(gen));
+
+    return ss.str();
+  }
+};
 
 #endif //SPACE_UTILS_H
