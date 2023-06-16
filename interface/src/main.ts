9a3f83baca94dc1242892f074f4434009f73df96
Capucinoxx <coub2802@usherbrooke.ca>
2023-06-16 10:53:21 -0400

    chore: open close modal

--- interface/src/main.ts
+++ interface/src/main.ts
@@ -1,6 +1,17 @@
 import { render_with_data } from './core/game'
 import { WebsocketService } from './service/websocket';
 
+// handle register modal events
+// ==================================================
+const register_modal = document.getElementById('register-modal') as HTMLElement;
+const register_modal_close = document.getElementById('register-modal-close') as HTMLElement;
+const register_modal_open = document.getElementById('register-modal-open') as HTMLElement;
+
+register_modal_open.onclick = () =>  { register_modal.style.display = 'block'; };
+register_modal_close.onclick = () => { register_modal.style.display = 'none'; };
+
+// Game management
+// ==================================================
 const board = document.getElementById('game-canvas') as HTMLCanvasElement;
 const scoreboard = document.getElementById('scores') as HTMLElement;
 const ctx = board.getContext('2d') as CanvasRenderingContext2D;
