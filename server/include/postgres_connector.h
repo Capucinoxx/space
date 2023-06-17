203990dc0b66b129fc8f43ce0964d8f232e050fe
Capucinoxx <coub2802@usherbrooke.ca>
2023-06-16 21:15:42 -0400

    chore: execute query with many args

--- server/include/postgres_connector.h
+++ server/include/postgres_connector.h
@@ -3,6 +3,7 @@
 
 #include "configuration.h"
 #include <pqxx/pqxx>
+
 #include <sstream>
 
 class PostgresConnector {
@@ -39,40 +40,21 @@ private:
 public:
 
   template<typename... Args>
-  pqxx::result execute_query(const std::string& query, Args&&... args) {
+  pqxx::result execute(const std::string& query, Args&&... args) {
     try {
       pqxx::work w(*connection);
 
-      pqxx::prepare::invocation invoc = w.prepared("custom_query");
-      set_parameters(invoc, 1, std::forward<Args>(args)...);
-
-      pqxx::result result = invoc.exec(query);
-
+      pqxx::result result = w.exec_params(query, std::forward<Args>(args)...);
       w.commit();
+
       return result;
     } catch(const std::exception &e) {
-      return pxqq::result();
+      return pqxx::result();
     }
-
-    pqxx::result result = w.exec(query);
-
-    w.exec(query);
-    w.commit();
-
-    return result;
   }
  
   bool connected() const noexcept {
     return connection->is_open();
-  }
-
-private:
-  void set_parameters(pqxx::prepare::invocation& invoc) {};
-
-  template<typename T, typename... Args>
-  void set_parameters(pqxx::prepare::invocation& invoc, int index, const T& value, Args&&... args) {
-    invoc("$" + std::to_string(index), value);
-    set_parameters(invoc, index + 1, std::forward<Args>(args)...);
   }
 };
 
