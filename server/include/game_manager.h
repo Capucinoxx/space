1ab55fb0bd94873eafde23e8a8fc5ffc278394a5
Capucinoxx <coub2802@usherbrooke.ca>
2023-06-18 02:48:13 -0400

    chore: cleanup game manager

--- server/include/game_manager.h
+++ server/include/game_manager.h
@@ -6,18 +6,11 @@
 #include "utils.h"
 #include "structures/concurrent_unordered_map.h"
 
-
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <array>
-#include <cmath>
-#include <random>
+#include <iostream> 
 #include <thread>
-#include <unordered_map>
-#include <functional>
-#include <memory>
 #include <atomic>
+#include <unordered_map>
+
 
 template<uint32_t ROWS, uint32_t COLS>
 class GameManager {
@@ -27,13 +20,10 @@ public:
 
 private:
   std::shared_ptr<Grid<ROWS, COLS>> grid;
-
-  ConcurrentUnorderedMap<uint32_t, std::shared_ptr<Player<ROWS, COLS>>> players{ };
-
-  std::vector<std::pair<uint32_t, uint32_t>> spawns;
-  std::atomic<int> current_spawn { 1 };
   std::shared_ptr<GameManager<ROWS, COLS>> game_manager;
 
+  ConcurrentUnorderedMap<uint32_t, std::shared_ptr<Player<ROWS, COLS>>> players{ };
+  
   std::thread th;
   uint32_t frame_count = 0;
   std::atomic<bool> running{ false };
@@ -42,7 +32,6 @@ private:
 
 public:
   explicit GameManager() { 
-    generate_spawns();
     grid = std::make_shared<Grid<ROWS, COLS>>();
   }
 
@@ -60,17 +49,14 @@ public:
     return true;
   }
 
-  bool is_running() const noexcept {
-    return running.load();
-  }
+  bool is_running() const noexcept { return running.load(); }
+  uint32_t frame() const noexcept { return frame_count; }
 
   void remove_player(std::shared_ptr<Player<ROWS, COLS>> p) {
     players.erase(p->id());
   }
 
-  std::string generate_secret() {
-    return uuid_generator();
-  }
+  std::string generate_secret() { return uuid_generator(); }
 
   template<typename T>
   void start(T* object, void (T::*callback)(const std::vector<uint8_t>&)) { 
@@ -80,7 +66,7 @@ public:
     running.store(true);
     th = std::thread([object, callback, this]() {
       while(running) {
-        update();
+        update_map();
         auto data = serialize();
         (object->*callback)(data);
 
@@ -94,22 +80,12 @@ public:
     if (th.joinable())
       th.join();
   }
-
-  void update() {
-    update_map();
-  }
-
-  uint32_t frame() const noexcept {
-    return frame_count;
-  }
-
+  
   std::shared_ptr<Grid<ROWS, COLS>> get_grid() const noexcept {
     return grid;
   }
 
   void spawn_player(std::shared_ptr<Player<ROWS, COLS>> p) {
-    // auto position = spawns[current_spawn++ % spawns.size()];
-
     auto position = std::make_pair(uint32_t(20), uint32_t(20));
 
     p->spawn(position);
@@ -118,9 +94,7 @@ public:
   void handle_move_result(std::shared_ptr<Player<ROWS, COLS>> p, Player<ROWS, COLS>::movement_type movement) {
     switch (movement) {
       case Player<ROWS, COLS>::movement_type::DEATH: {
-                auto victim_id = grid->at(p->pos()).get_value();
-
-        auto it = players.find(victim_id);
+        auto it = players.find(grid->at(p->pos()).get_value());
         if (it != players.end()) {
           auto victim = it->second;
           std::cout << "--- KILL by " << p->id() << " ---" << std::endl; 
@@ -132,25 +106,12 @@ public:
           if (victim->id() != p->id())
             std::cout << "is not the same" << std::endl;
         }
-      }
-
         break;
+      }
       case Player<ROWS, COLS>::movement_type::COMPLETE:
         p->fill_region();
         break;
     }
-  }
-
-  void kill() {
-    // TODO
-  }
-
-  void complete_area() {
-    // TODO
-  }
-
-  bool is_out_of_bounds() {
-    return false;
   }
 
   std::vector<uint8_t> serialize() {
@@ -171,55 +132,6 @@ private:
     for (auto& p : players)
       p.second->perform(frame_count);
     ++frame_count;
-  }
-
-  void generate_spawns() {
-    uint32_t num_spawns = std::sqrt(ROWS * COLS);
-    double min_distance = std::sqrt(static_cast<double>(ROWS * COLS)) / num_spawns;
-
-    std::mt19937 rng(std::random_device{}());
-    std::uniform_real_distribution<double> distribution(0.0, 1.0);
-
-    std::vector<std::vector<bool>> spawn_grid(ROWS, std::vector<bool>(COLS, false));
-    std::vector<position> active_spawns;
-
-    auto is_valid_point = [&](position p) {
-      if (p.first < COLS && p.second < ROWS)
-        return !spawn_grid[p.second][p.first];
-      return false;
-    };
-
-    auto add_point = [&](position p) {
-      active_spawns.push_back(p);
-      spawn_grid[p.second][p.first] = true;
-      spawns.push_back(p);
-    };
-
-    position start = {  static_cast<int>(distribution(rng) * COLS), 
-                        static_cast<int>(distribution(rng) * ROWS) };
-    add_point(start);
-    
-
-    while (!active_spawns.empty()) {
-      std::uniform_int_distribution<uint32_t> distribution(0, active_spawns.size() - 1);
-      uint32_t index = distribution(rng);
-      position active_pos = active_spawns[index];
-      active_spawns.erase(active_spawns.begin() + index);
-
-      const int num_attemps = 30;
-      for (int i = 0; i != num_attemps; ++i) {
-        double angle = 2.0 * M_PI * distribution(rng);
-        double distance = min_distance * (1.0 + distribution(rng));
-        uint32_t x = static_cast<uint32_t>(distance * std::cos(angle));
-        uint32_t y = static_cast<uint32_t>(distance * std::sin(angle));
-        
-        position new_pos = { active_pos.first + x, active_pos.second + y };
-        if (is_valid_point(new_pos)) {
-          add_point(new_pos);
-          active_spawns.push_back(new_pos);
-        }
-      }
-    }
   }
 };
 
