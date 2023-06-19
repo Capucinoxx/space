1f13d89891f1947333c837b1d7b37e2e8926fbc0
Benjamin Courchesne <42392217+Capucinoxx@users.noreply.github.com>
2023-06-19 15:43:06 -0400

    Fix - region trail length (#37)

--- server/include/player.h
+++ server/include/player.h
@@ -239,13 +239,13 @@ public:
     serialize_value<uint32_t>(data, pos().second);
     serialize_value<uint32_t>(data, frame_alive);
 
-    serialize_value<uint8_t>(data, static_cast<uint8_t>(trail.size()));
+    serialize_value<uint32_t>(data, static_cast<uint32_t>(trail.size()));
     for (auto it = trail.begin(); it != trail.end(); ++it) {
       serialize_value<uint32_t>(data, it->first);
       serialize_value<uint32_t>(data, it->second);
     }
 
-    serialize_value<uint32_t>(data, static_cast<uint8_t>(region.size()));
+    serialize_value<uint32_t>(data, static_cast<uint32_t>(region.size()));
     for (auto it = region.begin(); it != region.end(); ++it) {
       serialize_value<uint32_t>(data, it->first);
       serialize_value<uint32_t>(data, it->second);
