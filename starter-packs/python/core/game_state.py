3fe1886789a99dfebe865517d4df410fcb44ad38
Capucinoxx <bendertermebc@gmail.com>
2023-07-02 16:21:40 -0400

    chore: deserialize inacessible_tiles

--- starter-packs/python/core/game_state.py
+++ starter-packs/python/core/game_state.py
@@ -16,12 +16,19 @@ class GameState:
     rows: int
     cols: int
     players: List[Player]
+    inacessible_tiles: List[Tuple[int, int]]
 
     @classmethod
     def deserialize(cls, data: bytes) -> 'GameState':
         offset = 0
         rows, cols, frame = struct.unpack_from('<III', data, offset)
         offset += 12
+
+        inacessible_tiles_length = struct.unpack_from('<I', data, offset)[0]
+        offset += 4
+
+        inacessible_tiles = struct.unpack_from('<' + 'II' * inacessible_tiles_length, data, offset)
+        offset += inacessible_tiles_length * 8
 
         players = []
         while offset < len(data):
@@ -54,7 +61,7 @@ class GameState:
                               trail=list(zip(trail[::2], trail[1::2])), 
                               region=list(zip(region[::2], region[1::2]))))
 
-        return cls(rows=rows, cols=cols, players=players)
+        return cls(rows=rows, cols=cols, players=players, inacessible_tiles=inacessible_tiles)
     
     def __str__(self) -> str:
         return f"GameState(rows={self.rows}, cols={self.cols}, players={self.players})"
