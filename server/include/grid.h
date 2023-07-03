ce5cc2b30c5437c6f8184293f22ec947511bd818
Capucinoxx <bendertermebc@gmail.com>
2023-07-03 14:40:36 -0400

    fix: flood fill algorithm

--- server/include/grid.h
+++ server/include/grid.h
@@ -2,6 +2,7 @@
 #define SPACE_GRID_H
 
 #include <memory>
+#include <unordered_set>
 
 #include "tile_map.h"
 #include "player.h"
@@ -33,15 +34,16 @@ public:
     }
   }
 
-
   std::vector<uint32_t> fill_region(player_ptr p) {
     std::vector<uint32_t> killed{};
+    std::unordered_set<std::pair<uint32_t, uint32_t>, PairHash> trail{};
 
     std::size_t tile_coverage = 0;
 
     p->for_each_trail([&](const position& pos) {
       at(pos).take(p->id());
       ++tile_coverage;
+      trail.insert(pos);
       p->append_region({ pos });
     });
     p->clear_trail();
@@ -56,13 +58,16 @@ public:
       auto pos = neighbors.back();
       neighbors.pop_back();
 
-      if (is_invalid_pos(pos) || been[pos.first][pos.second] || grid[pos.first][pos.second].owner() == p->id())
+      if (is_invalid_pos(pos) || been[pos.first][pos.second] || trail.find(pos) != trail.end())
         continue;
 
       been[pos.first][pos.second] = true;
 
       ++tile_coverage;
       p->append_region({ pos });
+      auto [statement, old_owner] = at(pos).take(p->id());
+      if (statement == TileMap::stmt::DEATH && old_owner != p->id())
+        killed.insert(old_owner);
 
       auto movements = {
         std::make_pair(1, 0),
@@ -77,6 +82,13 @@ public:
 
         auto res = flood_fill(p->id(), { px, py }, been);
         ++++tile_coverage;
+
+        for (const auto& pos : res.second) {
+          auto [statement, old_owner] = at(pos).take(p->id());
+          if (statement == TileMap::stmt::DEATH && old_owner != p->id())
+            killed.insert(old_owner);
+        }
+
         p->append_region(res.second);
         killed.insert(killed.end(), res.first.begin(), res.first.end());
 
@@ -87,18 +99,15 @@ public:
     return killed;
   }
 
-    bool is_invalid_pos(const position& pos) const noexcept {
+  bool is_invalid_pos(const position& pos) const noexcept {
     return pos.first >= ROWS || pos.second >= COLS || inacessible_areas.find(pos) != inacessible_areas.end();
   }
 
 private:
-
-
   std::pair<std::unordered_set<uint32_t>, std::vector<position>> flood_fill(uint32_t self_id, const position& pos, std::array<std::array<bool, COLS>, ROWS>& been) {
-    if (is_invalid_pos(pos) || been[pos.first][pos.second] || at(pos).owner() == self_id)
+    if (is_invalid_pos(pos) || been[pos.first][pos.second])
       return { {}, {} };
 
-    bool surrounded = true;
     std::vector<position> neighbors{};
     neighbors.reserve(ROWS > COLS ? ROWS : COLS);
 
@@ -111,17 +120,17 @@ private:
       neighbors.pop_back();
 
       if (is_invalid_pos(pos)) {
-        surrounded = false;
-        continue;
+        std::cout << "invalid pos " << pos.first << " " << pos.second << std::endl;
+        return { {}, {} };
       }
 
-      if (been[pos.first][pos.second])
+      if (been[pos.first][pos.second] || at(pos).owner() == self_id)
         continue;
 
       been[pos.first][pos.second] = true;
 
-      if (surrounded)
-        filled.push_back(pos);
+      
+      filled.push_back(pos);
 
       neighbors.push_back({ pos.first + 1, pos.second });
       neighbors.push_back({ pos.first - 1, pos.second });
@@ -131,14 +140,11 @@ private:
 
     std::unordered_set<uint32_t> killed{};
 
-    if (surrounded) {
       for (auto& pos : filled) {
         auto [statement, old_owner] = grid[pos.first][pos.second].take(self_id);
         if (statement == TileMap::stmt::DEATH)
           killed.insert(old_owner);
       }
-    } else {
-      return { {}, {} };
     }
 
     return { killed, filled };
