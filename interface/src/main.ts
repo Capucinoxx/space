1d257c2c8f789ca44f15ed491131f95c159c519d
Capucinoxx <bendertermebc@gmail.com>
2023-07-08 18:24:18 -0400

    chore: redo game interface

--- interface/src/main.ts
+++ interface/src/main.ts
@@ -6,28 +6,28 @@ import { Canvas } from './core/canvas';
 
 // handle register modal events
 // ==================================================
-const register_modal = document.getElementById('register-modal') as HTMLElement;
-const register_modal_close = document.getElementById('register-modal-close') as HTMLElement;
-const register_modal_open = document.getElementById('register-modal-open') as HTMLElement;
-const register_form = register_modal.querySelector('form') as HTMLFormElement;
+// const register_modal = document.getElementById('register-modal') as HTMLElement;
+// const register_modal_close = document.getElementById('register-modal-close') as HTMLElement;
+// const register_modal_open = document.getElementById('register-modal-open') as HTMLElement;
+// const register_form = register_modal.querySelector('form') as HTMLFormElement;
 
-register_modal_open.onclick = () =>  { register_modal.style.display = 'block'; };
-register_modal_close.onclick = () => { register_modal.style.display = 'none'; };
+// register_modal_open.onclick = () =>  { register_modal.style.display = 'block'; };
+// register_modal_close.onclick = () => { register_modal.style.display = 'none'; };
 
-register_form.onsubmit = (e: SubmitEvent) => {
-    e.preventDefault();
-    const data = new FormData(register_form);
-    const hsl = HSL.from_hex(data.get('color') as string).to_array();
-    data.set('color', hsl.join(' '));
+// register_form.onsubmit = (e: SubmitEvent) => {
+//     e.preventDefault();
+//     const data = new FormData(register_form);
+//     const hsl = HSL.from_hex(data.get('color') as string).to_array();
+//     data.set('color', hsl.join(' '));
 
-    console.log(data);
+//     console.log(data);
 
-    send('http://localhost:8080/subscribe', 
-        { method: 'POST', body: data },
-        (response: ArrayBuffer) => {
-            alert(`secret: ${new TextDecoder().decode(response)}`);
-        });
-};
+//     send('http://localhost:8080/subscribe', 
+//         { method: 'POST', body: data },
+//         (response: ArrayBuffer) => {
+//             alert(`secret: ${new TextDecoder().decode(response)}`);
+//         });
+// };
 
 // Game management
 // ==================================================
