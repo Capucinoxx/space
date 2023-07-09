259f3bc87e895cb06b8e98e297feb0b2995dee86
Capucinoxx <bendertermebc@gmail.com>
2023-07-09 00:20:50 -0400

    chore: add rank option python start pack

--- starter-packs/python/run_bot.py
+++ starter-packs/python/run_bot.py
@@ -3,16 +3,20 @@ import argparse
 import asyncio
 
 
-DEFAULT_BASE_URL = "ws://localhost:8080/game"
+DEFAULT_BASE_URL = "ws://localhost:8080/unranked/game"
 
-async def loop(secret: str):
+async def loop(secret: str, url: str = DEFAULT_BASE_URL):
     await Socket(DEFAULT_BASE_URL, secret).run()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Starts the bot")
     parser.add_argument("-s", "--secret", help="The secret wich authenticates your bot", required=True)
+    parser.add_argument("-r", "--rank", action='store_true', help="If set, the bot will play ranked games")
 
     args = parser.parse_args()
 
-    asyncio.run(loop(args.secret))
+    if args.rank:
+        DEFAULT_BASE_URL = "ws://localhost:8080/ranked/game"
+
+    asyncio.run(loop(args.secret, DEFAULT_BASE_URL))
     
