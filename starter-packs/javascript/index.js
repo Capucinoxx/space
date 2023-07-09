8895f769d546b35f74ad2adfabd7dddcbc515743
Capucinoxx <bendertermebc@gmail.com>
2023-07-09 00:22:34 -0400

    chore: add rank option javascript start pack

--- starter-packs/javascript/index.js
+++ starter-packs/javascript/index.js
@@ -2,18 +2,22 @@ import { ArgumentParser } from 'argparse';
 
 import { Socket } from './network/network.js';
 
-const DEFAULT_BASE_URL = 'ws://localhost:8080/game';
+let DEFAULT_BASE_URL = 'ws://localhost:8080/unranked/game';
 
-async function loop(secret) {
-  await new Socket(DEFAULT_BASE_URL, secret).run();
+async function loop(secret, url) {
+  await new Socket(url, secret).run();
 }
 
 const parser = new ArgumentParser({ description: 'Starts the bot' });
 parser.add_argument('-s', '--secret', { help: 'The secret which authenticates your bot', required: true });
+parser.add_argument('-r', '--rank', { help: 'If set, the bot will play ranked game', action: 'store_true' });
 
 const args = parser.parse_args();
 
-loop(args.secret).catch((error) => {
+if (args.rank)
+  DEFAULT_BASE_URL = 'ws://localhost:8080/ranked/game';
+
+loop(args.secret, DEFAULT_BASE_URL).catch((error) => {
   console.error(error);
   process.exit(1);
 });
