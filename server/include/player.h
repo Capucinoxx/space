7a1ae4b94dcdc033d41b76b8ace708de588bbad7
Capucinoxx <coub2802@usherbrooke.ca>
2023-07-12 22:46:46 -0400

    chore: add player score functor

--- server/include/player.h
+++ server/include/player.h
@@ -14,6 +14,45 @@
 #include <tuple>
 #include <memory>
 
+template<uint32_t ROWS, uint32_t COLS>
+class PlayerScore {
+public:
+  uint64_t total_score;
+
+  std::tuple<uint64_t, uint64_t, uint64_t> tick_score;
+
+  explicit PlayerScore(uint64_t score) 
+    : total_score{ score }, tick_score{ {} } { }
+
+  uint64_t score() noexcept {
+    total_score += std::get<0>(tick_score) + std::get<1>(tick_score) + std::get<2>(tick_score);
+    tick_score = { 0, 0, 0 };
+    return total_score;
+  }
+
+  void add_zone_score(uint64_t n_tiles) noexcept {
+    std::get<0>(tick_score) += n_tiles;
+  }
+
+  void add_kill_score(uint64_t trail_size) noexcept {
+    std::get<1>(tick_score) += (12 * (1 + trail_size));
+  }
+
+  void add_capture_score(uint64_t trail_size) noexcept {
+    std::get<2>(tick_score) += (3 * (1 + trail_size));
+  }
+
+  std::ostream &operator<<(std::ostream &os) {
+    os  << " { total_score: " 
+        << total_score << ", tick_score: { zone_score: "  << std::get<0>(tick_score) 
+                                      << ", kill_score: " << std::get<1>(tick_score) 
+                                      << ", capture_score: " << std::get<2>(tick_score) << " } }";
+    return os;
+  }
+};
+
+// score = zone_score + kill_bonus + capture_zone
+
 struct PairHash {
   template <typename T1, typename T2>
   std::size_t operator()(const std::pair<T1, T2>& p) const {
@@ -41,7 +80,8 @@ private:
   uint32_t last_frame_played;
   uint32_t frame_alive;
   position current_pos;
-  float64_t p_score;
+  PlayerScore<ROWS, COLS> p_score;
+
   direction last_direction;
   hsl_color color;
   bool connected;
@@ -54,13 +94,21 @@ private:
   std::mutex mu;
 
 public:
-  Player(const std::string& name, uint32_t id, hsl_color color, float64_t score, uint32_t frame)
+  Player(const std::string& name, uint32_t id, hsl_color color, uint64_t score, uint32_t frame)
     : identifier{ id }, name{ name }, last_frame_played{ frame }, frame_alive{ 0 }, current_pos{}, p_score{ score }, last_direction{ direction::DOWN },
       color{ color }, connected{ true } {
     trail.reserve(ROWS * COLS / 2);
     region.reserve(MAX_SIZE * 2);
   }
 
+
+  void zone_captured_bonus() noexcept { p_score.add_capture_score(trail.size()); }
+  void kill_bonus() noexcept { p_score.add_kill_score(trail.size()); }
+
+  uint64_t tick_score() noexcept {
+    p_score.add_zone_score(region.size());
+    return p_score.score();
+  }
 
   std::string player_name() const noexcept { return name; }
   uint32_t id() const noexcept         { return identifier; }
