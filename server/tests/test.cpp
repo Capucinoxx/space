cb286dc323bc3bf2724f59521bf08f0b9a3ee991
jactymilena <saej3101@usherbrooke.ca>
2023-07-16 16:08:49 -0400

    chore: added weird pattern test

--- server/tests/test.cpp
+++ server/tests/test.cpp
@@ -167,6 +167,34 @@ int main() {
   }
   }});
 
+  scenarios.insert({ "weird pattern 3 - zone completion", {
+    Spawns{ position{ 2, 2 } },
+    Bots{ Bot{ id{ 1 }, position{ 2, 2 } } },
+    Ticks{
+      actions{ { id{ 1 }, DOWN  } }, actions{ { id{ 1 }, DOWN  } }, actions{ { id{ 1 }, DOWN  } }, actions{ { id{ 1 }, DOWN  } }, actions{ { id{ 1 }, DOWN  } },
+      actions{ { id{ 1 }, RIGHT } }, actions{ { id{ 1 }, RIGHT } }, actions{ { id{ 1 }, RIGHT } }, actions{ { id{ 1 }, RIGHT } }, 
+      actions{ { id{ 1 }, UP    } }, actions{ { id{ 1 }, LEFT  } }, actions{ { id{ 1 }, LEFT  } }, actions{ { id{ 1 }, LEFT  } },
+      actions{ { id{ 1 }, UP    } }, actions{ { id{ 1 }, RIGHT } }, actions{ { id{ 1 }, RIGHT } }, actions{ { id{ 1 }, RIGHT } },
+      actions{ { id{ 1 }, UP    } }, actions{ { id{ 1 }, LEFT  } }, actions{ { id{ 1 }, LEFT  } }, actions{ { id{ 1 }, LEFT  } }, actions{ { id{ 1 }, UP    } }
+    },
+    Expectations{
+      expectation{
+        id{ 1 },
+        position{ 3, 3 },
+        alive{ 22 },
+        scores{ 9, 18, 27, 36, 45, 54, 63, 72, 81, 90, 99, 108, 117, 126, 135, 144, 153, 162, 171, 180, 189, 281 },
+        trail_pos{},
+        region_pos{ position{ 1, 1 }, position{ 1, 2 }, position{ 1, 3 },
+                    position{ 2, 1 }, position{ 2, 2 }, position{ 2, 3 }, position{ 2, 4 }, position{ 2, 5 }, position{ 2, 6 }, position{ 2, 7 }, 
+                    position{ 3, 1 }, position{ 3, 2 }, position{ 3, 3 }, position{ 3, 4 }, position{ 3, 5 }, position{ 3, 6 }, position{ 3, 7 }, 
+                    position{ 4, 4 }, position{ 4, 5 }, position{ 4, 6 }, position{ 4, 7 }, 
+                    position{ 5, 4 }, position{ 5, 5 }, position{ 5, 6 }, position{ 5, 7 }, 
+                    position{ 6, 4 }, position{ 6, 5 }, position{ 6, 6 }, position{ 6, 7 }}
+    }
+  }
+  }});
+
+
   scenarios.insert({ "rectangle pattern 1 - zone completion", {
     Spawns{ position{ 2, 2 } },
     Bots{ Bot{ id{ 1 }, position{ 2, 2 } } },
