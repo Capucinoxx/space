0d7a7cb4f7511f653a54524c8064ea957880c1a3
Capucinoxx <bendertermebc@gmail.com>
2023-07-20 23:02:07 -0400

    fix: retrieve connexion when someone disconnect

--- server/src/network/shared_state.cpp
+++ server/src/network/shared_state.cpp
@@ -1,5 +1,6 @@
 #include "network/shared_state.h"
 #include "network/session.h"
+#include "common.h"
 
 shared_state::shared_state(std::string doc_root)
   : doc_root_(std::move(doc_root)) {}
@@ -11,13 +12,15 @@ void shared_state::join(websocket_session& session, std::string channel) {
 }
 
 void shared_state::leave(websocket_session& session) {
-  sessions_.do_at(session.get_channel(), [&session](auto &sessions) {
+  std::string channel = std::string(retrieve_channel(session.get_channel()));
+
+  sessions_.do_at(channel, [&session](auto &sessions) {
     sessions.erase(&session);
   });
 }
 
 void shared_state::send(const std::string& channel, const std::vector<uint8_t>& message) {
-  auto const ss = std::make_shared<std::vector<uint8_t> const>(std::move(message));
+  auto const ss = std::make_shared<std::vector<uint8_t> const>(message);
 
   sessions_.do_at(channel, [&ss](auto &sessions) {
     sessions.for_each([&ss](auto& session) {
