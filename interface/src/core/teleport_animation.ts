67cc84ea56343f71e94f89b499dbb2dd8b5017fc
Capucinoxx <bendertermebc@gmail.com>
2023-07-25 01:53:59 -0400

    feat: blue teleport pattern

--- interface/src/core/teleport_animation.ts
+++ interface/src/core/teleport_animation.ts
@@ -6,7 +6,32 @@ interface Rectangle {
   height: number
 };
 
-const blue_teleport = [] as Array<Rectangle>;
+const blue_teleport = [
+  { pos: { x: 1, y: 16 }, width: 1, height: 8 },
+  { pos: { x: 2, y: 12 }, width: 1, height: 15 },
+  { pos: { x: 3, y: 9 }, width: 1, height: 19 },
+  { pos: { x: 4, y: 5 }, width: 1, height: 9 },
+  { pos: { x: 5, y: 7 }, width: 1, height: 4 },
+  { pos: { x: 6, y: 5 }, width: 1, height: 3 },
+  { pos: { x: 7, y: 4 }, width: 1, height: 3 },
+  { pos: { x: 8, y: 3 }, width: 1, height: 3 },
+  { pos: { x: 9, y: 2 }, width: 1, height: 3 },
+  { pos: { x: 10, y: 1 }, width: 3, height: 3 },
+  { pos: { x: 13, y: 1 }, width: 1, height: 4 },
+  { pos: { x: 14, y: 2 }, width: 1, height: 4 },
+  { pos: { x: 15, y: 3 }, width: 1, height: 8 },
+  { pos: { x: 16, y: 4 }, width: 1, height: 18 },
+  { pos: { x: 17, y: 6 }, width: 1, height: 13 },
+  { pos: { x: 18, y: 8 }, width: 1, height: 5 },
+  { pos: { x: 4, y: 26 }, width: 1, height: 3 },
+  { pos: { x: 5, y: 27 }, width: 1, height: 3 },
+  { pos: { x: 6, y: 28 }, width: 1, height: 2 },
+  { pos: { x: 7, y: 29 }, width: 6, height: 1 },
+  { pos: { x: 9, y: 27 }, width: 4, height: 2 },
+  { pos: { x: 15, y: 19 }, width: 1, height: 6 },
+  { pos: { x: 14, y: 21 }, width: 1, height: 7 },
+  { pos: { x: 13, y: 23 }, width: 1, height: 5 },
+] as Array<Rectangle>;
 
 // { pos: { x: 0, y: 0 }, width: 0, height: 0 }
 const orange_teleport = [
