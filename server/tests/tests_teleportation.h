783817a631c164a961aa26668354dfc4f5d58dda
Capucinoxx <bendertermebc@gmail.com>
2023-07-26 10:48:53 -0400

    chore: add teleportation test edge case

--- server/tests/tests_teleportation.h
+++ server/tests/tests_teleportation.h
@@ -28,6 +28,56 @@ public:
         }
       }
     } });
+
+    scenarios.insert({ "teleporting two cnosecutive ticks", {
+      Spawns{ position{ 3, 3 } },
+      Bots{ Bot{ id{ 1 }, position{ 3, 3 } } },
+      Ticks{
+        actions{ { id{ 1 }, TELEPORT(2, 2) } },
+        actions{ { id{ 1 }, DOWN } },
+        actions{ { id{ 1 }, DOWN } },
+        actions{ { id{ 1 }, DOWN } },
+        actions{ { id{ 1 }, DOWN } },
+        actions{ { id{ 1 }, DOWN } },
+        actions{ { id{ 1 }, DOWN } },
+        actions{ { id{ 1 }, DOWN } },
+        actions{ { id{ 1 }, DOWN } },
+        actions{ { id{ 1 }, TELEPORT(4, 4) } },
+      },
+      Expectations{
+        expectation{
+          id{ 1 },
+          position{ 4, 4 },
+          alive{ 10 },
+          scores{ 9, 18, 27, 36, 45, 54, 63, 72, 81, 90 },
+          trail_pos{},
+          region_pos{ position{ 2, 2 }, position{ 2, 3 }, position{ 2, 4 },
+                      position{ 3, 2 }, position{ 3, 3 }, position{ 3, 4 },
+                      position{ 4, 2 }, position{ 4, 3 }, position{ 4, 4 } }
+        }
+      }
+    } });
+
+    scenarios.insert( { "teleport second time after 8 ticks", {
+      Spawns{ position{ 3, 3 } },
+      Bots{ Bot{ id{ 1 }, position{ 3, 3 } } },
+      Ticks{
+        actions{ { id{ 1 }, TELEPORT(2, 2) } },
+        actions{ { id{ 1 }, TELEPORT(4, 4) } },
+      },
+      Expectations{
+        expectation{
+          id{ 1 },
+          position{ 2, 2 },
+          alive{ 2 },
+          scores{ 9, 18 },
+          trail_pos{},
+          region_pos{ position{ 2, 2 }, position{ 2, 3 }, position{ 2, 4 },
+                      position{ 3, 2 }, position{ 3, 3 }, position{ 3, 4 },
+                      position{ 4, 2 }, position{ 4, 3 }, position{ 4, 4 } }
+        }
+      }
+    } });
   }
 };
 
