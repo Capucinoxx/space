86e23ddcb97d57dad8a7e31c471753374f606ec2
Capucinoxx <bendertermebc@gmail.com>
2023-07-26 19:28:15 -0400

    chore: add custom url python bot

--- starter-packs/python/run_bot.py
+++ starter-packs/python/run_bot.py
@@ -2,21 +2,24 @@ from network.network import Socket
 import argparse
 import asyncio
 
+DEFAULT_BASE_URL = "ws://jdis-ia.dinf.fsci.usherbrooke.ca"
 
-DEFAULT_BASE_URL = "ws://localhost:8080/unranked/game"
-
-async def loop(secret: str, url: str = DEFAULT_BASE_URL):
-    await Socket(DEFAULT_BASE_URL, secret).run()
+async def loop(secret: str, url: str):
+    await Socket(url, secret).run()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Starts the bot")
     parser.add_argument("-s", "--secret", help="The secret wich authenticates your bot", required=True)
     parser.add_argument("-r", "--rank", action='store_true', help="If set, the bot will play ranked games")
+    parser.add_argument("-u", "--url", help="The url of the server", default=DEFAULT_BASE_URL)
 
     args = parser.parse_args()
 
-    if args.rank:
-        DEFAULT_BASE_URL = "ws://localhost:8080/ranked/game"
+    args.url = args.url.strip()
 
-    asyncio.run(loop(args.secret, DEFAULT_BASE_URL))
+    channel = "/unranked/game"
+    if args.rank:
+        channel = "/ranked/game"
+
+    asyncio.run(loop(args.secret, args.url + channel))
     
