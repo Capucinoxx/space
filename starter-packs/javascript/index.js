72958b4d2a08c075edfeece56cc7903d82faa3c5
Capucinoxx <bendertermebc@gmail.com>
2023-07-26 19:39:24 -0400

    chore: add custom url starter pack js

--- starter-packs/javascript/index.js
+++ starter-packs/javascript/index.js
@@ -2,7 +2,7 @@ import { ArgumentParser } from 'argparse';
 
 import { Socket } from './network/network.js';
 
-let DEFAULT_BASE_URL = 'ws://localhost:8080/unranked/game';
+const DEFAULT_BASE_URL = 'ws://localhost:8080/unranked/game';
 
 async function loop(secret, url) {
   await new Socket(url, secret).run();
@@ -11,13 +11,17 @@ async function loop(secret, url) {
 const parser = new ArgumentParser({ description: 'Starts the bot' });
 parser.add_argument('-s', '--secret', { help: 'The secret which authenticates your bot', required: true });
 parser.add_argument('-r', '--rank', { help: 'If set, the bot will play ranked game', action: 'store_true' });
+parser.add_argument('-u', '--url', { help: 'The url of the game server', default: DEFAULT_BASE_URL });
 
 const args = parser.parse_args();
+args.url = args.url.trim();
+
+let channel = '/unranked/game';
 
 if (args.rank)
-  DEFAULT_BASE_URL = 'ws://localhost:8080/ranked/game';
+  channel = '/ranked/game';
 
-loop(args.secret, DEFAULT_BASE_URL).catch((error) => {
+loop(args.secret, args.url + channel).catch((error) => {
   console.error(error);
   process.exit(1);
 });
